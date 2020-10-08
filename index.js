584592d6324247521a7e3ec89ce3b55c3f432ff5
Andrew <agcummings11@gmail.com>
2020-10-08 15:53:02 -0700

    env vars, mysql

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+
+const connection = require("mysql")
+connection.createConnection({
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD
+});
 
 app.use(express.static("public"));
 
 app.get("/api/test", (req, res) => res.send("test is working!"));
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, () => console.log("Listening on port 3000"));
